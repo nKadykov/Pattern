bbfc941c5ab98f789d524d0074e0f59ce8907e1d
nKadykov <nkadykov@gmail.com>
2024-04-09 08:56:12 +0300

    init

---
+++ Singleton.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Singleton {
+public:
+    static Singleton &getInstance() {
+        if(!instance) {
+            instance = new Singleton();
+        }
+        return *instance;
+    }
+    void someOperation() {
+        std::cout << "Singleton is performing some operation." << std::endl;
+    }
+    Singleton(const Singleton &) = delete;
+    Singleton operator=(const Singleton &) = delete;
+private:
+    Singleton() {
+        std::cout << "Singleton instance created." << std::endl;
+    }
+    ~Singleton() {
+        std::cout << "Singleton instance destroyed." << std::endl;
+    }
+    static Singleton *instance;
+};
+
+Singleton *Singleton::instance = nullptr;
+
+int main() {
+    Singleton &singleton = Singleton::getInstance();
+    singleton.someOperation();
+    return 0;
+}
